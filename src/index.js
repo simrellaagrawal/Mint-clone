a77faf3749d679a9d63fef22fbaa9b3713f4b176
Simrella Agrawal <95346529+simrellaagrawal@users.noreply.github.com>
2023-01-08 14:21:49 +0530

    Revert "Develop"

--- src/index.js
+++ src/index.js
@@ -2,18 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { store } from './store/store'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistStore } from 'redux-persist'
+import 'tw-elements';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let persistor = persistStore(store)
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </React.StrictMode>
-  </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,66 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
     extend: {
-      width: {
-        '550': '550px',
-        '245': '245px',
-        '437px': '437px',
-        'inherit': 'inherit',
-      },
-      height: {
-        '445': '445px',
-        '1px': '1px',
-        '2px': '2px',
-        'inherit': 'inherit',
-      },
-      backgroundColor: {
-        'e3e3e3': "#e3e3e3",
-        'semitransparent': '#00000057',
-        '1313137e': '#1313137e',
-        '0000003d': "#0000003d"
-      },
-      backgroundImage: {
-        'caroselImg': "url('https://images.livemint.com/dev/ico_caraousel2.svg')"
-      },
-      fontFamily: {
-        'Lato': "'Lato', sans-serif"
-      },
-      zIndex: {
-        '-1': '-1',
-        '-2': '-2',
-        '60': '60',
-      },
-      backdropBlur: {
-        '100': '100px'
-      },
-      borderColor: {
-        "f4dd09ff": '#f4dd09ff',
-        "d0d0d05e": '#d0d0d05e',
-        '909090': '#909090',
-        'f1f1f1': '#f1f1f1',
-        '#f6f6f6': '#f6f6f6'
-      },
-      borderWidth: {
-        '1px': '1px',
-        '2px': '2px',
-        '3px': '3px',
-        '4px': '4px'
-      },
-      colors: {
-        'FFFFFF': '#FFFFFF',
-        '212121': '#212121',
-        '474747': '#474747',
-        '363636': '#363636',
-        '777777': '#777777'
-
-      },
-      minHeight: {
-        '293': '293px'
-      },
       screens: {
-        md: { 'max': "768px" },
+        mobileMd: "375px",
         mobileLg: "425px",
         tablet: "768px",
         laptopSm: "800px",
@@ -71,8 +18,7 @@ module.exports = {
         desktopMd: "1440px",
         desktopLg: "1920px",
       },
-
     },
+    plugins: [require("tw-elements/dist/plugin")],
   },
-  plugins: [],
-}
+};
